319edbdc8b730ea4310ab8b3f66df127dd310550
Ronan <ronan.moriarty@justeattakeaway.com>
2021-08-18 08:05:41 +0100

    Assignment 1

--- section-2/assignment-1/pages/index.vue
+++ section-2/assignment-1/pages/index.vue
@@ -3,6 +3,7 @@
     <ol>
       <li>Create two routes: /products and /products/:pid</li>
       <li>Add a button to the main page. The button should navigate (programmatically) to /products</li>
+      <button @click="$router.push('/products')">Products</button>
       <li>Add two links to the /products page. The links should go to /products/1 and /products/2</li>
       <li>Output the product id on the /products/:pid page</li>
     </ol>
---
+++ section-2/assignment-1/pages/products/_pid/index.vue
@@ -0,0 +1,6 @@
+<template>
+    <section>
+        <h2>Product Page</h2>
+        <p>Product Id: {{ $route.params.pid }}</p>
+    </section>
+</template>
